24a629ae9aaf318692f650f17e77920b98c86a54
yogiykv <72684700+yogiykv@users.noreply.github.com>
2020-10-21 07:33:00 +0530

    login page

---
+++ login/script.js
@@ -0,0 +1,14 @@
+
+
+// function validateForm() {
+//   var name = document.forms["loginPage"]["name"].value;
+//   var pass = document.forms["loginPage"]["pass"].value;
+//   if (name == "YOGESH" && pass= "12345678") {
+//   alert("Hello! I am an alert box11!!");
+//   }
+//   else
+//   {
+//   alert("Hello! I am an alert box!!");
+//   }
+// }
+
